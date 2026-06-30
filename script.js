582c289646e62fe8f37ef9e4a2f748cb09c67303
aub48 <aubproject05@gmail.com>
2026-06-30 14:58:56 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("AmbuTrack fixed version");
+
+const validID = "3538273";
+
+document.getElementById("startBtn").addEventListener("click", ()=>{
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+      ()=>alert("লোকেশন নেওয়া হয়েছে"),
+      ()=>alert("লোকেশন অনুমতি দিন")
+    );
+  }
+});
+
+document.getElementById("confirmBtn").addEventListener("click", ()=>{
+  const val = document.getElementById("idBox").value.trim();
+  const msg = document.getElementById("msg");
+
+  if(val === validID){
+    window.location.href = "https://maps.app.goo.gl/K8W7C17oAxzBCyS6A?g_st=ic";
+  } else {
+    msg.innerText = "আপনি রেজিস্ট্রারড নন";
+  }
+});
+
+document.getElementById("trafficBtn").addEventListener("click", ()=>{
+  document.getElementById("traffic").classList.toggle("hidden");
+});
